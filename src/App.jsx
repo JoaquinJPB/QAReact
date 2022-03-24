3bde3fca9a35be9051b4aa9d26c49021903128c6
JoaquinJPB <jjpb2000@gmail.com>
2022-03-24 14:17:48 +0000

    Q&A Panel

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,22 @@ import data from "./data";
 import SingleQuestion from "./Question";
 
 function App() {
+
+    const [questions,setQuestions] = useState(data);
+    
     return (
-            <h2>accordion project setup</h2>
+            <main>
+                <div className="container">
+                    <h3>Questions and answers about login</h3>
+                    <section className="info">
+                        {
+                            questions.map((question) => {
+                                return <SingleQuestion key={question.id} {...question} />
+                            })
+                        }
+                    </section>
+                </div>
+            </main>
             );
 }
 
--- src/Question.jsx
+++ src/Question.jsx
@@ -2,9 +2,20 @@ import React, { useState } from 'react';
 
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-const Question = () => {
+const Question = ({title, info}) => {
+
+    const [showInfo, setShowInfo] = useState(false);
+
     return (
-            <h2>question component</h2>
+            <article className="question">
+                <header>
+                    <h4>{title}</h4>
+                    <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+                        {showInfo? <AiOutlineMinus /> : <AiOutlinePlus />}
+                    </button>
+                </header>
+                {showInfo && <p>{info}</p>}
+            </article>
             );
 };
 
